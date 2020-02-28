2cb69f2b7e774ec8fc8aaf1c2f998370d12d95f3
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-28 23:56:13 +0900

    Finish 51

--- dist/class.js
+++ dist/class.js
@@ -1,16 +1,16 @@
 "use strict";
 class Person {
-    constructor(initName) {
+    constructor(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
+    }
+    incrementAge() {
+        this.age += 1;
     }
     greeting() {
-        console.log(`Hello! My name is ${this.name}`);
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
     }
 }
-const quill = new Person('Quill');
+const quill = new Person('Quill', 38);
+quill.incrementAge();
 quill.greeting();
-const anotherQuill = {
-    name: 'anotherQuill',
-    anotherGreeting: quill.greeting,
-};
-anotherQuill.anotherGreeting();
