81de684cd7af22eafa14a61e23db6c0d5b068c00
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-29 00:20:59 +0900

    Finish 56

--- src/class.ts
+++ src/class.ts
@@ -9,7 +9,19 @@ class Person {
 }
 
 class Teacher extends Person {
-  constructor(name: string, age: number, public subject: string) {
+  get subject() {
+    if (!this._subject) {
+      throw new Error('There is no subject.');
+    }
+    return this._subject;
+  }
+  set subject(value) {
+    if (!value) {
+      throw new Error('There is no subject.');
+    }
+    this._subject = value;
+  }
+  constructor(name: string, age: number, private _subject: string) {
     super(name, age);
   }
   greeting(this: Teacher) {
@@ -20,4 +32,5 @@ class Teacher extends Person {
 }
 
 const teacher = new Teacher('Quill', 38, 'Math');
+teacher.subject = 'Music';
 teacher.greeting();
