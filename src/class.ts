3aae78c348073e74d9ed13c4aae66c2cf7296a83
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-29 00:41:35 +0900

    Finish 59

--- src/class.ts
+++ src/class.ts
@@ -15,6 +15,7 @@ abstract class Person {
 }
 
 class Teacher extends Person {
+  private static instance: Teacher;
   explainJob() {
     console.log(`I am a teacher and I teach ${this.subject}.`);
   }
@@ -30,10 +31,17 @@ class Teacher extends Person {
     }
     this._subject = value;
   }
-  constructor(name: string, age: number, private _subject: string) {
+  private constructor(name: string, age: number, private _subject: string) {
     super(name, age);
+  }
+  static getInstance() {
+    if (!Teacher.instance) {
+      Teacher.instance = new Teacher('Quill', 38, 'Math');
+    }
+    return Teacher.instance;
   }
 }
 
-const teacher = new Teacher('Quill', 38, 'Math');
-teacher.greeting();
+const teacher = Teacher.getInstance();
+const teacher2 = Teacher.getInstance();
+console.log(teacher, teacher2);
