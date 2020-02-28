1c21ec944eacd8c098b4fb2bc4edf808e1b6faba
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-28 22:26:52 +0900

    Finish 42

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,2 +1,14 @@
-let hi = 32;
-console.log(hi.toUpperCase());
+function echo(message: string): string | null {
+  return message;
+}
+let implicitAny;
+implicitAny = 'implicitAny';
+
+let nullableMesssage: string | null = echo('hi');
+let undefinedMesssage: string | undefined = undefined;
+let onlyNull: null = null;
+let onlyUndefined: undefined = undefined;
+if (nullableMesssage) {
+  nullableMesssage.toUpperCase();
+}
+const newFunction = echo.bind(null, 'hi');
