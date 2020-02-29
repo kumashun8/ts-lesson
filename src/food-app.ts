6bf65705b7324d62255459b8f0071cf63fb4bd24
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-03-01 00:24:32 +0900

    Finish 119

--- src/food-app.ts
+++ src/food-app.ts
@@ -2,7 +2,7 @@ class Score {}
 
 class Food {
   constructor(public element: HTMLDivElement) {
-    element.addEventListener('click', this.clickEventHandler);
+    element.addEventListener('click', this.clickEventHandler.bind(this));
   }
   clickEventHandler() {
     this.element.classList.toggle('food--active');
