9bd955084b4d60c7f50361ec3ddf9567f824102f
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-03-01 00:18:17 +0900

    Finish 116

---
+++ dist/food-app.js
@@ -0,0 +1,12 @@
+"use strict";
+class Score {
+}
+class Food {
+}
+class Foods {
+    constructor() {
+        this.elements = document.querySelectorAll('.food');
+        this.elements.forEach(element => { });
+    }
+}
+const foods = new Foods();
