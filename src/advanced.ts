20e3a799da79c87cc512d79e4544bf9d90c9f289
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-29 15:50:12 +0900

    Finish 84

--- src/advanced.ts
+++ src/advanced.ts
@@ -35,6 +35,18 @@ const upperHello: TmpFunc = function(x: string | number) {
   return 0;
 };
 
+interface FuncA {
+  (a: number, b: string): number;
+  (a: string, b: number): number;
+}
+interface FuncB {
+  (a: string): number;
+}
+let intersectionFunc: FuncA & FuncB;
+intersectionFunc = function(a: number | string, b?: number | string) {
+  return 0;
+};
+
 type NomadWorker = Engineer | Blogger;
 function describeProfile(nomadWorker: NomadWorker) {
   console.log(nomadWorker.name);
