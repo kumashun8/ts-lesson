ffd384b4140883a71215bbed0df8609972ff7cc2
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-29 18:48:48 +0900

    Finish 104

--- dist/decorator.js
+++ dist/decorator.js
@@ -5,9 +5,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logging(constructor) {
-    console.log('Logging...');
-    console.log(constructor);
+function Logging(message) {
+    return function (constructor) {
+        console.log(message);
+        console.log(constructor);
+    };
+}
+function Component(template, selector) {
+    return function (constructor) {
+        const mountedElement = document.querySelector(selector);
+        const instance = new constructor();
+        if (mountedElement) {
+            mountedElement.innerHTML = template;
+            mountedElement.querySelector('h1').textContent = instance.name;
+        }
+    };
 }
 let User = class User {
     constructor() {
@@ -16,7 +28,8 @@ let User = class User {
     }
 };
 User = __decorate([
-    Logging
+    Component('<h1>{{ name }}</h1>', '#app'),
+    Logging('Logging User')
 ], User);
 const user1 = new User();
 const user2 = new User();
