9374e4ddafe67074f84b70a220e690ebc9be8dd5
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-29 17:06:54 +0900

    Finish 95

--- src/generics.ts
+++ src/generics.ts
@@ -5,4 +5,23 @@ function copy<T extends { name: string }, U extends keyof T>(
   value[key];
   return value;
 }
-console.log(copy({ name: 'Quill', age: 38 }, 'name'));
+// console.log(copy({ name: 'Quill', age: 38 }, 'name'));
+
+class LightDatabase<T extends string | number | boolean> {
+  private data: T[] = [];
+  add(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  get() {
+    return this.data;
+  }
+}
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Banana');
+console.log(stringLightDatabase.get());
