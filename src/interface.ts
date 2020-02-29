6a1907fd666e1d95c30be35297199267e3581935
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-29 12:24:05 +0900

    Finish 65

--- src/interface.ts
+++ src/interface.ts
@@ -4,16 +4,21 @@ interface Human {
   greeting(message: string): void;
 }
 class Developer implements Human {
-  constructor(public name: string, public age: number) {}
+  constructor(
+    public name: string,
+    public age: number,
+    public experience: number
+  ) {}
   greeting(message: string) {
-    console.log('Hello!');
+    console.log(message);
   }
 }
-
-const human = {
+const tmpDeveloper = {
   name: 'Quill',
   age: 38,
+  experience: 3,
   greeting(message: string) {
     console.log(message);
   },
 };
+const user: Human = tmpDeveloper;
